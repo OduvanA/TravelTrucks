50fa4557606afb52036ebd907ded0cf4de3e8bab
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-12-10 22:35:50 -0600

    Update CampersPage.jsx

--- src/pages/CampersPage/CampersPage.jsx
+++ src/pages/CampersPage/CampersPage.jsx
@@ -5,14 +5,18 @@ import { fetchCampers } from "../../redux/operations";
 import FiltersForm from "../../components/FiltersForm/FiltersForm";
 import css from './CampersPage.module.css';
 import { resetFilters } from "../../redux/filtersSlice";
+import { useLocation } from "react-router-dom";
 
 export default function CampersPage() {
   const dispatch = useDispatch();
+  const location = useLocation();
+  console.log(location);
+  
   
   useEffect(() => {
     dispatch(resetFilters());
     dispatch(fetchCampers({page: 1, limit: 4}));
-  }, [dispatch]);
+  }, [dispatch, location]);
 
   return (
     <div className={css.container}>
